84b90c1da92903cd05adacefed5bfe6bd83128f0
busraS <busrasozudogru@gmail.com>
2015-05-26 13:54:20 +0300

    ana sayfa son

--- app/src/main/java/com/example/user/ornekler/AnaSayfa.java
+++ app/src/main/java/com/example/user/ornekler/AnaSayfa.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by USER on 22.5.2015.
@@ -13,6 +14,14 @@ public class AnaSayfa extends Activity {
     Button btnyemekekleanasyfa;
     Button btnyemeklistesianasayfa;
     Button btnsiparisanasayfa;
+    Button btnyorumlaranasayfa;
+    Button btncikisanasayfa;
+    Button btnguncelleanasayfa;
+    TextView emailanasayfatextview;
+    TextView telnoanasyfatextview;
+    TextView yasadigiyeranasayfatextview;
+    TextView adresanasayfatextview;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,47 @@ public class AnaSayfa extends Activity {
         btnsiparisanasayfa =(Button)findViewById(R.id.btnsiparisanasyfa);
         btnyemekekleanasyfa = (Button)findViewById(R.id.btnyemekekleanasayfa);
         btnyemeklistesianasayfa = (Button)findViewById(R.id.btnyemeklistesianasyfa);
+        btnguncelleanasayfa = (Button)findViewById(R.id.btnguncelleanasyfa);
+        btnyorumlaranasayfa = (Button)findViewById(R.id.btnyorumlaranasayfa);
+        btncikisanasayfa = (Button)findViewById(R.id.btncikisanasayfa);
+        emailanasayfatextview = (TextView)findViewById(R.id.emailanasyfatextview);
+        telnoanasyfatextview = (TextView)findViewById(R.id.telnoanasayfatextview);
+        yasadigiyeranasayfatextview =(TextView)findViewById(R.id.yasadigiyeranasyfatextview);
+        adresanasayfatextview = (TextView)findViewById(R.id.adresanasyfatextview);
+
+
+        btnyorumlaranasayfa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btnsiparisanasayfa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        btnguncelleanasayfa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  intent = new Intent(AnaSayfa.this ,YeniActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        btncikisanasayfa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
         btnyemekekleanasyfa.setOnClickListener(new View.OnClickListener() {
             @Override
