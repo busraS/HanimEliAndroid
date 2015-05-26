a9045cda407a32ca2c8f2ce59eed86e5434be6bb
busraS <busrasozudogru@gmail.com>
2015-05-26 15:41:08 +0300

    yemek kayıt son

---
+++ app/src/main/java/com/example/user/ornekler/YorumActivity.java
@@ -0,0 +1,19 @@
+package com.example.user.ornekler;
+
+import android.app.Activity;
+import android.widget.TextView;
+
+/**
+ * Created by USER on 26.5.2015.
+ */
+public class YorumActivity extends Activity {
+
+    TextView kullaniciyorumtextview;
+    TextView tarihyorumtextview;
+    TextView yorumyorumtextview;
+
+
+
+
+
+}
